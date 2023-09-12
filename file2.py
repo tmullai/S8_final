2b054bd40db7c2d5fd3f148ea5f9be470c09e1b4
tmullai <119833796+tmullai@users.noreply.github.com>
2023-09-12 20:33:24 +0530

    Create file2.py

---
+++ file2.py
@@ -0,0 +1,14 @@
+# Python program to find the maximum of two numbers
+
+
+def maximum(a, b):
+    if a >= b:
+        return a
+    else:
+        return b
+
+ 
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+print("Maximum value = ", maximum(a, b))
+
